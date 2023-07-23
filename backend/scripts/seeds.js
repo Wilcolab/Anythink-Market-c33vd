b5dff3c328fd463753076b7fc9f508daf5e79ef3
Md Abid Hussain <101964499+md-abid-hussain@users.noreply.github.com>
2023-07-23 18:45:27 +0000

    updated seeds.js

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const connection = process.env.MONGODB_URI;
 mongoose.connect(connection);
 
-require("./models/User");
-require("./models/Item");
-require("./models/Comment");
+require("../models/User");
+require("../models/Item");
+require("../models/Comment");
 const User = mongoose.model("User");
 const Item = mongoose.model("Item");
 const Comment = mongoose.model("Comment");
